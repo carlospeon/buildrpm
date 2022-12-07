287fe47f8b8350242f0b24a7fb45442b59a32b70
Carlos Peon Costa <carlospec@inditex.com>
2022-12-07 17:36:26 +0100

    add release var and output

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,11 @@
 
 set -eo pipefail
 
-version="$INPUT_VERSION"
 spec="$INPUT_SPEC"
-release="1"
+version="$INPUT_VERSION"
+release="$INPUT_RELEASE"
 
-target="${PWD}/.rpmbuild"
+target=".rpmbuild"
 no_clean=""
 
 dist="$(rpm --eval '%{?dist}')"
@@ -18,13 +18,13 @@ mkdir -p ${target}/{SOURCES,BUILD,RPMS,SRPMS,BUILDROOT,SPECS}
 git archive --output=${target}/SOURCES/collectd-${version}.tar.gz \
   --prefix=collectd-${version}/ HEAD
 
-rpmbuild --define "_topdir ${target}" \
+rpmbuild --define "_topdir ${PWD}/${target}" \
   --define "version ${version}" --define "release ${release}" -bs ${spec}
 
 dnf config-manager --set-enabled devel
 dnf builddep -y ${target}/SRPMS/${srpm}
 
-rpmbuild --define "_topdir ${target}" \
+rpmbuild --define "_topdir ${PWD}/${target}" \
   --define "version ${version}" --define "release ${release}" -bb ${spec}
 
-find ${target}
+echo "rpms_path=${target}/RPMS" >> $GITHUB_OUTPUT
